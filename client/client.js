2eab0ef278e807dda4af0165881073befb9bb3ba
Nuwan <nuwan@beehyyv.com>
2016-10-29 01:51:48 +0530

    header

---
+++ client/client.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, Redirect, hashHistory} from 'react-router';
+import Homepage from '../pages/Home';
+import Homepage2 from '../pages/Home2';
+import App from '../pages/App';
+/*import App from '../components/App';*/
+import configureStore from '../redux/store';
+import { Provider } from 'react-redux';
+require("../css/style.css");
+
+let initialState = {
+  todos: [{
+    id: 0,
+    completed: false,
+    text: 'Initial todo for demo purposes'
+  }],
+  user: {
+    username: 'Nuwan',
+    id: 13
+  }
+}
+
+let store = configureStore(initialState)
+render(
+  <Provider store={store}>
+     <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Homepage}></IndexRoute>
+            <Route path="page2" component={Homepage2}></Route>
+        </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
---
+++ pages/Home2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link } from "react-router";
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="home_content">
+        home2
+      </div>
+    );
+  }
+}
